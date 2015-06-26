f409e15f724da6dfe942080745467a0ecfba8bd6
Vladimir Skok <skoksworks@gmail.com>
2015-06-26 00:51:54 -0700

    Add content.php, update functions

---
+++ site/web/app/themes/alpha-framework/content.php
@@ -0,0 +1,40 @@
+<?php
+/**
+* The default template for displaying content
+*/
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<!-- Article header -->
+	<header class="class-entry">
+		<?php
+		// If the post has a thumbnail and it's not password protected
+		// then display the thumbnail
+		if ( has_post_thumbnail() && ! post_password_required() ) : ?>
+			<figure class="entry-thumbnail">
+				<?php the_post_thumbnail(); ?>
+			</figure>
+		<?php endif;
+
+		// If single page, display the title
+		// Else, we display the title in a link
+		if ( is_single() ) : ?>
+			<h1>
+				<?php the_title(); ?>
+			</h1>
+		<?php else : ?>
+			<h1>
+				<a href="<?php the_permalink(); ?>" rel="bookmark">
+					<?php the_title(); ?>
+				</a>
+			</h1>
+		<?php endif; ?>
+
+		<p class="entry-meta">
+			<?php
+				// Display the meta information
+				alpha_post_meta();
+			?>
+		</p>
+	</header>
+</article>
